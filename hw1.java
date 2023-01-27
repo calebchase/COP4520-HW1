47c5e87b29c857b5a77a59ebe7edc304a4ca6a57
Caleb Chase <calebchase0308@gmail.com>
2023-01-27 18:45:08 -0500

    clean up and comments

--- hw1.java
+++ hw1.java
@@ -5,12 +5,14 @@ import java.util.concurrent.*;
 
 public class hw1 {
     public static void main(String[] args) {
+        // set max number of threads to 8
         ExecutorService exService = Executors.newFixedThreadPool(8);
         long primeSum = 0;
         long primeCount = 0;
         long startTime;
         long endTime;
         int maxNum = (int) Math.pow(10, 8);
+        // maxNum + 1 to be able to index array with maxNum: sieve[maxNum]
         boolean[] sieve = new boolean[maxNum + 1];
         int[] maxPrimes = new int[10];
 
@@ -18,25 +20,28 @@ public class hw1 {
         startTime = System.currentTimeMillis();
 
         // find primes using sieve of eratosthenes
-        for (int i = 2; i <= Math.sqrt(maxNum); i++) {
-           if (sieve[i] == false) exService.execute(new Ascend(sieve, i, maxNum));
-        }
+        for (int i = 2; i <= Math.sqrt(maxNum); i++)
+            if (sieve[i] == false)
+                // runs Ascend() using executor service thread pool
+                exService.execute(new Ascend(sieve, i, maxNum));
 
         // wait for threads to finish
         exService.shutdown();
         try {
             exService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
 
         // get total primes, sum of primes, and largest primes
-        for (int i = maxNum; i >= 2 ; i--) {
-            if(sieve[i] != false) continue;
+        for (int i = maxNum; i >= 2; i--) {
+            if (sieve[i] != false)
+                continue;
 
-            if(primeCount < 10) maxPrimes[(int) primeCount] = i;
-        
+            // tracks 10 largest primes found
+            if (primeCount < 10)
+                maxPrimes[(int) primeCount] = i;
+
             primeSum += i;
             primeCount++;
         }
@@ -44,6 +49,7 @@ public class hw1 {
         // END
         endTime = System.currentTimeMillis();
 
+        // write results to file called primes.txt
         try {
             File outFile = new File("primes.txt");
             outFile.createNewFile();
@@ -51,17 +57,20 @@ public class hw1 {
 
             // Display results, (time / 1000) -> ms to s
             outFileWriter.write(((endTime - startTime) / 1000.0) + " " + primeCount + " " + primeSum);
+            outFileWriter.write("\n");
+
+            // write largest primes from lowest to highest order
             for (int i = maxPrimes.length - 1; i >= 0; i--)
                 outFileWriter.write(Integer.toString(maxPrimes[i]) + "\n");
 
             outFileWriter.close();
-        } 
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e);
-        }        
+        }
     }
 }
 
+// class run in parallel in threads using java executor service
 class Ascend implements Runnable {
     boolean[] sieve;
     int i;
@@ -73,10 +82,10 @@ class Ascend implements Runnable {
         this.maxNum = maxNum;
     }
 
+    // segment from the sieve of eratosthenes
     public void run() {
         int base = i * i;
-        for (int j = 0; base + i*j <= maxNum; j++) {
-            sieve[base + i*j] = true;       
-        }
+        for (int j = 0; base + i * j <= maxNum; j++)
+            sieve[base + i * j] = true;
     }
 }
